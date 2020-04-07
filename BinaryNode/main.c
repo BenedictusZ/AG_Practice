d99aa0acbef2ecc703b58156dd0cd99d5b57e3a5
tom <mozheng8@live.com>
2020-04-07 20:54:34 +0800

    2020.4.7 night
    Signed-off-by: tom <mozheng8@live.com>

--- BinaryNode/main.c
+++ BinaryNode/main.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+//TODO 求叶结点的数目
 typedef struct BinaryNode {
 	char ch;
 	struct BINARYNODE *lchild;
@@ -18,16 +20,29 @@ void Recursion(BinaryNode *root) {
 	}
 
 	//访问根结点
-	printf("%c", root->ch);
+	Recursion(( BinaryNode * ) root->ch);
 	//再遍历左子树
 	Recursion(( BinaryNode * ) root->lchild);
 	//再遍历右子树
 	Recursion(( BinaryNode * ) root->rchild);
-
 }
 
-void CresteBinaryTree() {
+void CaculateLeafNum(BinaryNode *root, int *leafNum) {
+	if ( root == NULL) {
+		return;
+	}
 
+	if ( root->lchild == NULL && root->rchild == NULL) {
+		(*leafNum)++;
+	}
+	//左子树叶子节点的数目
+	CaculateLeafNum(root->lchild, leafNum);
+	//右子树叶子结点的数目
+	CaculateLeafNum(root->rchild, leafNum);
+}
+
+
+void CresteBinaryTree() {
 	//创建节点
 	BinaryNode node1 = {'A', NULL, NULL};
 	BinaryNode node2 = {'B', NULL, NULL};
@@ -39,17 +54,17 @@ void CresteBinaryTree() {
 	BinaryNode node8 = {'H', NULL, NULL};
 
 	//建立节点关系
-	node1.lchild = ( struct BINARYNODE * ) &node2;
-	node1.rchild = ( struct BINARYNODE * ) &node6;
-	node2.rchild = ( struct BINARYNODE * ) &node3;
-	node3.lchild = ( struct BINARYNODE * ) &node4;
-	node3.rchild = ( struct BINARYNODE * ) &node5;
-	node6.rchild = ( struct BINARYNODE * ) &node7;
-	node7.lchild = ( struct BINARYNODE * ) &node8;
+	node1.lchild = &node2;
+	node1.rchild = &node6;
+	node2.rchild = &node3;
+	node3.lchild = &node4;
+	node3.rchild = &node5;
+	node6.rchild = &node7;
+	node7.lchild = &node8;
 
-	//递归遍历
-	Recursion(&node1);
-	printf("\n");
+	int leafNum = 0;
+	CaculateLeafNum(&node1, &leafNum);
+	printf("leaf node nums:\n", &leafNum);
 
 };
 
